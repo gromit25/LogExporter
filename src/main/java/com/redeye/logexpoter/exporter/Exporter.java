fb3d11bb257704d29948c55ab4816fc009ac8967
jung mo <gromit25@hanmail.net>
2025-07-02 11:24:50 +0900

    Update Exporter.java

--- src/main/java/com/redeye/logexpoter/exporter/Exporter.java
+++ src/main/java/com/redeye/logexpoter/exporter/Exporter.java
@@ -6,5 +6,11 @@ package com.redeye.logexpoter.exporter;
  * @author jmsohn
  */
 public abstract class Exporter {
-
+  
+  /**
+   * repository로 메시지 전송
+   *
+   * @param message 전송할 메시지 
+   */
+  public abstract void send(String message) throws Exception;
 }
