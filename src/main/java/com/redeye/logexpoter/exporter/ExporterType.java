8208a5b8318c2aaffea0c06571fc6f5ec576ce75
jung mo <gromit25@hanmail.net>
2025-07-02 14:59:30 +0900

    Update ExporterType.java

--- src/main/java/com/redeye/logexpoter/exporter/ExporterType.java
+++ src/main/java/com/redeye/logexpoter/exporter/ExporterType.java
@@ -6,5 +6,25 @@ package com.redeye.logexpoter.exporter;
  * @author jmsohn
  */
 public enum ExporterType {
-	KAFKA;
+	
+	PRINT {
+		@Override
+		public Exporter create() {
+			return new PrintExporter();
+		}
+	},
+	
+	KAFKA {
+		@Override
+		public Exporter create() {
+			return new KafkaExpoter();
+		}
+	};
+
+	/**
+	 * 각 타입별 Exporter 생성 후 반환
+	 *
+	 * @return 생성된 Expoter
+	 */
+	public abstract Expoter create();
 }
