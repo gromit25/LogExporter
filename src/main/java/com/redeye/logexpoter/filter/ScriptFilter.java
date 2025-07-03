7190369beeae6d849bfe4dec4e4dc747acf1ab32
jung mo <gromit25@hanmail.net>
2025-07-03 10:07:02 +0900

    Update ScriptFilter.java

--- src/main/java/com/redeye/logexpoter/filter/ScriptFilter.java
+++ src/main/java/com/redeye/logexpoter/filter/ScriptFilter.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import com.jutools.StringUtil;
 import com.jutools.script.olexp.OLExp;
@@ -18,7 +17,6 @@ import jakarta.annotation.PostConstruct;
  * 
  * @author jmsohn
  */
-@Component
 public class ScriptFilter implements LogFilter {
 	
 	/** */
