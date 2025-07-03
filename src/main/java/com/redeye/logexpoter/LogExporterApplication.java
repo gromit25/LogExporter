4c96fcb6ba921e9401504db5b34895d350c94aa5
gromit25 <gromit25@hanmail.net>
2025-07-03 22:43:59 +0900

    개발 중

--- src/main/java/com/redeye/logexpoter/LogExporterApplication.java
+++ src/main/java/com/redeye/logexpoter/LogExporterApplication.java
@@ -1,19 +1,14 @@
 package com.redeye.logexpoter;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.StandardWatchEventKinds;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.jutools.FileUtil;
 
 /**
  * 
@@ -43,78 +38,14 @@ public class LogExporterApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		System.out.println("DEBUG 100");
-
 		// log tracking 시작
 		Thread thread = new Thread(this.logExporter);
 		thread.start();
 
 		// stop 파일이 생성되거 업데이트 될때까지 대기
-		waitForFileTouched(this.stopFile, 1000L);
+		FileUtil.waitForFileTouched(this.stopFile);
 
 		// stop 파일 생성시 logExporter 중지
 		this.logExporter.stop();
-
-		System.out.println("DEBUG 200");
-	}
-
-	/**
-	 * stop 파일이 생성되거나 업데이트 될때까지 대기
-	 *
-	 * @param stopFile stop 파일
-	 * @param pollingPeriod 폴링 시간
-	 */
-	private static void waitForFileTouched(File stopFile, long pollingPeriod) throws Exception {
-
-		// 입력값 검증
-		if(stopFile == null) {
-			throw new IllegalArgumentException("stop file is null.");
-		}
-
-		if(pollingPeriod < 100L) {
-			throw new IllegalArgumentException("polling period must be greater than 100: " + pollingPeriod);
-		}
-
-		// stop 파일명
-		String stopFileName = stopFile.getName();
-		
-		// 파일 생성 및 업데이트 이벤트 수신을 위한 Watch 서비스 생성 및 등록
-		Path parentPath = stopFile.toPath().getParent();
-		WatchService parentWatchService = parentPath.getFileSystem().newWatchService();
-		parentPath.register(parentWatchService
-			, StandardWatchEventKinds.ENTRY_CREATE
-			, StandardWatchEventKinds.ENTRY_MODIFY
-			);
-
-		while(true) {
-			
-			// WatchKey에 이벤트 들어올 때까지 대기
-			WatchKey watchKey = parentWatchService.poll(pollingPeriod, TimeUnit.MILLISECONDS);
-			if(watchKey == null) {
-				continue;
-			}
-
-			try {
-				
-				// 생성 및 업데이트 이벤트 목록 확인
-				List<WatchEvent<?>> eventList = watchKey.pollEvents();
-				for (WatchEvent<?> event : eventList) {
-			
-					WatchEvent.Kind<?> kind = event.kind();   // 이벤트 종류
-					Path eventFile = (Path) event.context();  // 이벤트가 발생한 파일
-
-					// 파일이 새로 생성되거나 업데이트(ex. touch 명령어)되면 대기 중지
-					if(kind == StandardWatchEventKinds.ENTRY_CREATE || kind == StandardWatchEventKinds.ENTRY_MODIFY) {
-						if(stopFileName.equals(eventFile.toFile().getName()) == true) {
-							return;
-						}
-					}
-				}
-				
-			} finally {
-				// WatchKey 초기화 꼭 해줘야 함
-				watchKey.reset();
-			}
-		} // End of while
 	}
 }
