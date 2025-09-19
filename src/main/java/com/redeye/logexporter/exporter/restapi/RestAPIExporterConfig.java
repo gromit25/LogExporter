bdc8fa5bfbe119ba01725cc8eb17fc03b4ba6981
jung mo <gromit25@hanmail.net>
2025-09-19 17:57:44 +0900

    Create RestAPIExporterConfig.java

---
+++ src/main/java/com/redeye/logexporter/exporter/restapi/RestAPIExporterConfig.java
@@ -0,0 +1,34 @@
+package com.redeye.logexporter.exporter.restapi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@ConditionalOnProperty
+(
+	value = "app.exporter.type",
+	havingValue = "RESTAPI"
+)
+public class RestAPIExporterConfig {
+	
+	/**
+	 * WebClient 객체 생성 및 반환
+	 *
+	 * @param url 기본 url
+  	 * @return 생성된 WebClient 객체
+	 */
+	@Bean("webClient")
+	WebClient webClient(
+		@Value("app.exporter.restapi.url") String url
+	) {
+		return WebClient.builder()
+			.baseUrl(url)
+			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+			.build();
+	}
+}
