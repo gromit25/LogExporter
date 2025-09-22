392161463991d333d4f12d3f01efe4ab9c451dba
jung mo <gromit25@hanmail.net>
2025-09-22 10:41:11 +0900

    Update TraceDTO.java

--- src/main/java/com/redeye/logexporter/exporter/restapi/domain/TraceDTO.java
+++ src/main/java/com/redeye/logexporter/exporter/restapi/domain/TraceDTO.java
@@ -1,8 +1,22 @@
 package com.redeye.logexporter.exporter.restapi;
 
+@Data
 public class TraceDTO {
+
+  /** */
   private long start;
+
+  /** */
   private long end;
-  private List<JoinPointDTO> joinPointList;
+
+  /** */
+  private Map<String, JoinPointDTO> joinPointList = new ConcurrentHashMap<>();
+
+  /** */
   private List<LinkDTO> linkList;
+
+  
+  public TraceDTO add(String message) throws Exception {
+    return this;
+  }
 }
