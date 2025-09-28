c573ac60a27e46d44f67130cdda8ad8df8f68661
gromit25 <gromit25@hanmail.net>
2025-09-28 09:19:23 +0900

    크론 핸들러 실행 클래스 개발

--- src/main/java/com/redeye/logexporter/workflow/CronHandlerRunner.java
+++ src/main/java/com/redeye/logexporter/workflow/CronHandlerRunner.java
@@ -1,30 +1,55 @@
 package com.redeye.logexporter.workflow;
 
-import org.springframework.beans.factory.annotation.Value;
-
 import com.jutools.CronJob;
+import com.redeye.logexporter.workflow.comp.CronHandler;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 크론 핸들러 실행 클래스
  * 
  * @authro jmsohn
  */
+@Slf4j
 public class CronHandlerRunner extends AbstractRunner {
 	
-	/** */
+	/** 크론 잡 */
 	private CronJob job;
 	
 	
 	/**
+	 * 생성자
 	 * 
-	 * 
-	 * @param period
+	 * @param period 크론잡 주기
 	 */
-	public CronHandlerRunner(@Value("${}") String period) {
+	public CronHandlerRunner(String period) throws Exception {
 		
+		// 크론 잡 생성
+		this.job = new CronJob(period, () -> {
+			
+			try {
+				getComponent(CronHandler.class).flush();
+			} catch(Exception ex) {
+				log.error("", ex);
+				putNotice(ex);
+			}
+		});
+		
+		// 크론 잡 수행
+		this.job.run();
 	}
 
 	@Override
 	protected void processData() throws Exception {
+		
+		// 입력 큐에서 메시지를 가져옴
+		// 없을 경우 즉시 반환
+		Message<?> message = this.poll();
+		if(message == null) {
+			return;
+		}
+		
+		// 메시지 처리
+		this.getComponent(CronHandler.class).accept(message);
 	}
 }
