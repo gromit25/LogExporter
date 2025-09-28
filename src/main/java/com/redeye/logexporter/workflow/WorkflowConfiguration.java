09835297e24a9a16731813e5368f58a76cd878f9
gromit25 <gromit25@hanmail.net>
2025-09-28 16:43:38 +0900

    워크플로두 개발 중

--- src/main/java/com/redeye/logexporter/workflow/WorkflowConfiguration.java
+++ src/main/java/com/redeye/logexporter/workflow/WorkflowConfiguration.java
@@ -1,5 +1,6 @@
 package com.redeye.logexporter.workflow;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -33,9 +34,22 @@ public class WorkflowConfiguration {
 	 * 
 	 * @return 생성된 워크플로우
 	 */
-	@Bean
+	@Bean("workflow")
 	Workflow workflow() throws Exception {
-		//TODO workflow 생성 코드 작성 예정
-		return null;
+		
+		// 런너 맵
+		Map<String, AbstractRunner> map = new HashMap<>();
+		
+		// 런너 생성 -------------
+		// 컴포넌트 생성
+		// 컴포넌트의 런너 생성 및 설정
+		
+		// 런너 링킹 -------------
+		
+		// 워크플로우 생성 및 반환
+		Workflow wf = new Workflow();
+		wf.setRunnerMap(map);
+		
+		return wf;
 	}
 }
