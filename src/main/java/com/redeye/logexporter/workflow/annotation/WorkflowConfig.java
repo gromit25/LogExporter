fb8037de63813b5003710e30ba433ed4e2123517
jung mo <gromit25@hanmail.net>
2025-09-30 14:25:23 +0900

    Update WorkflowConfig.java

--- src/main/java/com/redeye/logexporter/workflow/annotation/WorkflowConfig.java
+++ src/main/java/com/redeye/logexporter/workflow/annotation/WorkflowConfig.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME) 
 @Target(ElementType.TYPE)
 public @interface WorkflowConfig {
-  String from() default null;
-  String subscribe() default null;
+  String from() default "";
+  String subscribe() default "";
   int threadCount() default 1;
 }
