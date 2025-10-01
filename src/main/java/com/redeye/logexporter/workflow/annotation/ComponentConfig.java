49e2ff073564c0aad8e0ec2149f2c2af55c59009
jung mo <gromit25@hanmail.net>
2025-10-01 13:56:20 +0900

    Update and rename WorkflowConfig.java to ComponentConfig.java

--- src/main/java/com/redeye/logexporter/workflow/annotation/ComponentConfig.java
+++ src/main/java/com/redeye/logexporter/workflow/annotation/ComponentConfig.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME) 
 @Target(ElementType.TYPE)
-public @interface WorkflowConfig {
+public @interface ComponentConfig {
   String from() default "";
   String subscribe() default "";
   int threadCount() default 1;
