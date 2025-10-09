e259a9f24eeb0f6c4915c4a192888de276e7fdab
jmsohn <gromit25@hanmail.net>
2025-10-09 19:25:47 +0900

    수정본

--- src/main/java/com/redeye/logexporter/common/exporter/kafka/KafkaExporter.java
+++ src/main/java/com/redeye/logexporter/common/exporter/kafka/KafkaExporter.java
@@ -1,6 +1,7 @@
 package com.redeye.logexporter.common.exporter.kafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
 
@@ -11,7 +12,11 @@ import com.jutools.workflow.annotation.Proc;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Kafka 출력 Exporter
+ * Kafka 출력 Exporter<br>
+ * 설정값<br>
+ * <li>app.common.kafka.use: 'y' 일 경우 활성화</li>
+ * <li>app.common.kafka.from: 이전 액티비티 명</li>
+ * <li>app.common.kafka.topic: kafka topic</li>
  * 
  * @author jmsohn
  */
@@ -30,6 +35,10 @@ public class KafkaExporter {
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
 	
+	/** Kafka 전송 토픽명 */
+	@Value("${app.common.kafka.topic}")
+	private String topic;
+	
 	
 	/**
 	 * Kafka 로 메시지 전송
@@ -40,6 +49,6 @@ public class KafkaExporter {
 	public void export(Message<?> message) throws Exception {
 		
 		log.info("SEND:" + message);
-		this.kafkaTemplate.send(message.getTopic(), message.getBody().toString());
+		this.kafkaTemplate.send(this.topic, message.getBody().toString());
 	}
 }
