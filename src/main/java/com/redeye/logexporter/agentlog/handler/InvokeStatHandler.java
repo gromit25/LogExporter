3af7899ae45bdaf041caebdd6a3c8fc3e736955b
jung mo <gromit25@hanmail.net>
2025-10-10 13:25:58 +0900

    Update InvokeStatHandler.java

--- src/main/java/com/redeye/logexporter/agentlog/handler/InvokeStatHandler.java
+++ src/main/java/com/redeye/logexporter/agentlog/handler/InvokeStatHandler.java
@@ -1,5 +1,22 @@
 package com.redeye.logexporter.agentlog.handler;
 
-public class InvokeStatHandler {
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
+import com.jutools.workflow.annotation.Activity;
+
+/**
+ * 
+ * 
+ * @author jmsohn
+ */
+@Activity(
+	value="",
+	from=""
+)
+@ConditionalOnProperty(
+	name="app.appagent.tracker.use",
+	havingValue="y"
+)
+public class InvokeStatHandler {
+  
 }
